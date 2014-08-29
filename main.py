1db0b70c8de9237ff7aa218054acd5c72bbed196
Shashank Srivastav <shashankgrovy@gmail.com>
2014-08-30 04:15:29 +0530

    remove jibberish and add authentication code + commands

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 import os
-
+import sys
 import dropbox
 
 # keys are set in virtualenv's postactivate script
@@ -11,33 +11,100 @@ app_secret = os.environ.get('APP_SECRET')
 access_token = os.environ.get('ACCESS_TOKEN')
 user_id = os.environ.get('USER_ID')
 
-### To get access token
+def get_access(self):
+    """Get access token and enable client access."""
 
-# flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+    authorize_url = flow.start()
+    print '1. Go to: ' + authorize_url
+    print '2. Click "Allow" (you might have to log in first)'
+    print '3. Copy the authorization code.'
+    code = raw_input("Enter the authorization code here: ").strip()
 
-# authorize_url = flow.start()
-# print '1. Go to: ' + authorize_url
-# print '2. Click "Allow" (you might have to log in first)'
-# print '3. Copy the authorization code.'
-# code = raw_input("Enter the authorization code here: ").strip()
+    try:
+        access_token, user_id = flow.finish(code)
+    except rest.ErrorResponse, e:
+        self.stdout.write('Error: %s\n' % str(e))
+        return
 
-# access_token, user_id = flow.finish(code)
+    print('Recieved access_token: %s\n User_id: %s\n', %s(access_token
+                                                          user_id))
 
-# print(access_token, user_id)
+    ### enable client access
+    with open(self.TOKEN_FILE, 'w') as f:
+        f.write('oauth2:' + access_token)
+    self.api_client = dropbox.client.DropboxClient(access_token)
 
-### client access
 
-client = dropbox.client.DropboxClient(access_token)
+def log(msg):
+    """print to screen."""
+    print msg
 
-f = open('main.py', 'rb')
-response = client.put_file('./the_source.py', f, overwrite=True)
-print('Uploaded:\n', response)
 
-file_metadata = client.metadata('./the_source.py')
-print('\n\nMetadata:\n', file_metadata)
+### Commands
 
-f, metadata = client.get_file_and_metadata('./the_source.py')
-out = open('sample.py', 'wb')
-out.write(f.read())
-out.close()
-print('\n\nDownloaded:\n', metadata)
+def drp_ls():
+    """list files/folders in current directory(default:root)"""
+    pass
+
+def drp_cd():
+    """navigate inside the directories."""
+    pass
+
+def drp_tree():
+    """show file structure of current directory"""
+    pass
+
+def drp_bulk_upload(*files):
+    """upload file(s) to current directory or destination path.
+
+    > drp up file1 file2 file3 -d [dest_path]
+
+    Usage:
+      drp up testfile
+      drp up testfile -d DropBox/sampleFolder/newfile
+    """
+    #TODO: add destination parameters
+
+    log("Preparing file for upload ...")
+    for filename in files:
+        with open(filename) as f:
+            response = client.put_file(filename, f, overwrite=True)
+            log("Uploading file ...")
+    print('Uploaded:\n', response)
+
+def drp_download():
+    """download file(s) to current directory or destination_path.
+
+    > drp down file1 file2 file3 -d [dest_path]
+    Usage:
+      drp down testfile
+      drp down testfile -d Desktop/newfile
+    """
+    #f, metadata = client.get_file_and_metadata('./the_source.py')
+    #out = open('sample.py', 'wb')
+    #out.write(f.read())
+    #out.close()
+    #print('\n\nDownloaded: \n', metadata)
+    pass
+
+def drp_rm():
+    pass
+
+def drp_share():
+    """copy public URL of source_file_path to clipboard"""
+    pass
+
+def drp_fileinfo():
+    #file_metadata = client.metadata('./the_source.py')
+    #print('\n\nMetadata:\n', file_metadata)
+    pass
+
+
+def main():
+    if APP_KEY == '' or APP_SECRET == '':
+        exit("You need to set your APP_KEY and APP_SECRET!")
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
